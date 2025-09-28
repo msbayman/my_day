217f6a474385db75e3ca79125c816c5627a467b1
miyamoto <aymanmsaoub@gmail.com>
2025-09-28 20:06:22 +0100

    start front settings

--- front_diary/app/routes/(main)/layout.tsx
+++ front_diary/app/routes/(main)/layout.tsx
@@ -1,15 +1,16 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
-
 export default function Layout({ children }: { children: React.ReactNode }) {
    return (
       <SidebarProvider>
-         <AppSidebar />
-         <main>
-            <SidebarTrigger />
-            {children}
-         </main>
+         <div className="flex min-h-screen w-full">
+            <AppSidebar />
+            <main className="flex-1 w-full">
+               <SidebarTrigger />
+               {children}
+            </main>
+         </div>
       </SidebarProvider>
    )
 }
--- front_diary/app/routes/(main)/settings/page.tsx
+++ front_diary/app/routes/(main)/settings/page.tsx
@@ -1,9 +1,33 @@
+"use client"
 import React from 'react'
-
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
 const settings = () => {
   return (
-    <div>settings</div>
+    <div className='flex flex-col w-full items-center justify-center  h-full p-8 gap-4'>
+      <h1 className='text-4xl font-bold'>Settings</h1>
+      <h1>ayman@test.com</h1>
+      <br />
+      <br />
+      <form className="flex flex-col gap-4 " >
+        <Label htmlFor="Username">Username</Label>
+        <Input type="text" id="Username" placeholder='Enter your name' />
+        <Label htmlFor="Newpassword">New Password</Label>
+        <Input type="password" id="Newpassword" placeholder='Enter your New password' />
+        <div className="2fa flex gap-3">
+
+          <Label  htmlFor="2Fa">2FA</Label>
+          <Checkbox />
+        </div>
+        <Label htmlFor="Currentpassword">Current Password</Label>
+        <Input type="password" id="Currentpassword" placeholder='Enter your Current password' />
+        <Button type="submit">Save</Button>
+      </form>
+    </div>
   )
 }
+
 
 export default settings
